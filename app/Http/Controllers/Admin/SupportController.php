90a6fb99f58874eb1a8230f37abc1eb1fcd9e9bd
Joel Fragoso <joelfragoso85@icloud.com>
2023-07-29 12:47:22 -0300

    feat: add crud supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,70 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin.supports.index');
+        $supports = $support->all();
+        return view('admin.supports.index', compact('supports'));
+    }
+
+    public function show(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+
+        return view('admin.supports.show', compact('support'));
+    }
+
+    public function create()
+    {
+        return view('admin.supports.create');
+    }
+
+    public function store(Request $request, Support $support)
+    {
+        $data = $request->all();
+        $data['status'] = 'a';
+        $support->create($data);
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+
+        return view('admin.supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject',
+            'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
     }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,27 @@
 <h1>Listagem dos suportes</h1>
+
+<a href="{{ route('supports.create') }}">Criar dúvida</a>
+
+<table>
+    <thead>
+        <tr>
+            <th>assunto</th>
+            <th>status</th>
+            <th>descrição</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    <a href="{{ route('supports.show', $support->id) }}">Visualizar</a>
+                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
